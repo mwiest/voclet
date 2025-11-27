950c2f2d678dbc8cefd695cfca1d2eb13601a8ac
Marc Wiest <m.wiest@gmx.ch>
2025-11-27 23:20:31 +0100

    feat: Uppercase delete button text
    Converts the "Delete" text on the button in the `WordListDetailScreen` to uppercase to match the Material Design text button style.

--- app/src/main/java/com/github/mwiest/voclet/ui/wordlist/WordListDetailScreen.kt
+++ app/src/main/java/com/github/mwiest/voclet/ui/wordlist/WordListDetailScreen.kt
@@ -38,7 +38,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.input.key.onPreviewKeyEvent
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
